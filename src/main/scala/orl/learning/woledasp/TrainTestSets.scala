d678876136abc73600a555e63b25a2f49540308e
nkatz <nkatz@iit.demokritos.gr>
2020-02-25 02:20:57 +0200

    trying different test sets

--- src/main/scala/orl/learning/woledasp/TrainTestSets.scala
+++ src/main/scala/orl/learning/woledasp/TrainTestSets.scala
@@ -169,7 +169,7 @@ object MeetingTrainTestSets {
       "caviar-video-14-meeting-moving", "caviar-video-9", "caviar-video-10",
       "caviar-video-19-meeting-moving", "caviar-video-11", "caviar-video-12-moving",
       "caviar-video-20-meeting-moving", "caviar-video-15", "caviar-video-16",
-      "caviar-video-21-meeting-moving", "caviar-video-17", "caviar-video-18",
+      "caviar-video-21-meeting-moving", "caviar-video-30", "caviar-video-18",
       "caviar-video-22-meeting-moving", "caviar-video-23-moving", "caviar-video-25",
       "caviar-video-24-meeting-moving", "caviar-video-26", "caviar-video-27",
       "caviar-video-28-meeting", "caviar-video-29", "caviar-video-4")
@@ -190,7 +190,13 @@ object MeetingTrainTestSets {
       "caviar-video-28-meeting", "caviar-video-5", "caviar-video-30")
      */
     // This ordering seems to be giving good results in terms of the rules it learns
-    Vector("caviar-video-21-meeting-moving", "caviar-video-11", "caviar-video-6", "caviar-video-3", "caviar-video-8", "caviar-video-24-meeting-moving", "caviar-video-10", "caviar-video-25", "caviar-video-1-meeting-moving", "caviar-video-27", "caviar-video-16", "caviar-video-28-meeting", "caviar-video-18", "caviar-video-5", "caviar-video-14-meeting-moving", "caviar-video-13-meeting", "caviar-video-30", "caviar-video-20-meeting-moving", "caviar-video-22-meeting-moving", "caviar-video-9", "caviar-video-17", "caviar-video-12-moving", "caviar-video-15", "caviar-video-26", "caviar-video-7", "caviar-video-19-meeting-moving", "caviar-video-23-moving")
+    Vector("caviar-video-21-meeting-moving", "caviar-video-11", "caviar-video-6", "caviar-video-3",
+      "caviar-video-8", "caviar-video-24-meeting-moving", "caviar-video-10", "caviar-video-25",
+      "caviar-video-1-meeting-moving", "caviar-video-27", "caviar-video-16", "caviar-video-28-meeting",
+      "caviar-video-4", "caviar-video-5", "caviar-video-14-meeting-moving", "caviar-video-13-meeting",
+      "caviar-video-30", "caviar-video-20-meeting-moving", "caviar-video-22-meeting-moving", "caviar-video-9",
+      "caviar-video-17", "caviar-video-12-moving", "caviar-video-15", "caviar-video-26", "caviar-video-7",
+      "caviar-video-19-meeting-moving", "caviar-video-23-moving")
   private val test2 = Vector("caviar-video-2-meeting-moving", "caviar-video-29", "caviar-video-18")
 
   val meeting2: (Vector[String], Vector[String]) = (train2, test2)
@@ -252,7 +258,7 @@ object MeetingTrainTestSets {
   val meeting6: (Vector[String], Vector[String]) = (train6, test6)
 
   private val train7 =
-    Vector("caviar-video-1-meeting-moving", "caviar-video-3", "caviar-video-4",
+    Vector("caviar-video-1-meeting-moving", "caviar-video-3", "caviar-video-18",
       "caviar-video-2-meeting-moving", "caviar-video-5", "caviar-video-6",
       "caviar-video-13-meeting", "caviar-video-7", "caviar-video-8",
       "caviar-video-14-meeting-moving", "caviar-video-17", "caviar-video-10",
@@ -266,7 +272,7 @@ object MeetingTrainTestSets {
   val meeting7: (Vector[String], Vector[String]) = (train7, test7)
 
   private val train8 =
-    Vector("caviar-video-1-meeting-moving", "caviar-video-3", "caviar-video-4",
+    Vector("caviar-video-1-meeting-moving", "caviar-video-17", "caviar-video-3", "caviar-video-4",
       "caviar-video-2-meeting-moving", "caviar-video-5", "caviar-video-6",
       "caviar-video-13-meeting", "caviar-video-7", "caviar-video-8",
       "caviar-video-14-meeting-moving", "caviar-video-9", "caviar-video-25",
@@ -274,7 +280,7 @@ object MeetingTrainTestSets {
       "caviar-video-20-meeting-moving", "caviar-video-15", "caviar-video-16",
       "caviar-video-21-meeting-moving", "caviar-video-23-moving", "caviar-video-18",
       "caviar-video-24-meeting-moving", "caviar-video-26", "caviar-video-27",
-      "caviar-video-28-meeting", "caviar-video-29", "caviar-video-30")
+      "caviar-video-28-meeting", "caviar-video-29")
   private val test8 = Vector("caviar-video-22-meeting-moving", "caviar-video-10", "caviar-video-30")
   //"caviar-video-17"
   val meeting8: (Vector[String], Vector[String]) = (train8, test8)
